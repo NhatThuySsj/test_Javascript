e04790ef2459082b69b2e682e97a2280750b9a47
NhatThuySsj <166314655+NhatThuySsj@users.noreply.github.com>
2024-05-17 13:58:35 +0700

    test_17_5

--- test_2.js
+++ test_2.js
@@ -160,86 +160,39 @@
 //     console.log(i)
 // }
 
-//Bài tập in ra những giá trị khác nhau trong mảng
+// VD
 
-//Test 1 in số lượng phần tử khác
+// var course = [
+//   {
+//     id: 1,
+//     name: 'SS',
+//     coin: 100
+//   },
+//   {
+//     id: 2,
+//     name: 'AA',
+//     coin: 160
+//   },
+//   {
+//     id: 3,
+//     name: 'EE',
+//     coin: 130
+//   },
+//   {
+//     id: 4,
+//     name: 'DD',
+//     coin: 400
+//   },
+//   {
+//     id: 5,
+//     name: 'QQ',
+//     coin: 120
+//   },
+// ]
 
-// function countDistinctValues(arr) {
-//     let distinctCount = 0;
-//     const seen = new Set();
-  
-//     for (let i = 0; i < arr.length; i++) {
-//       if (!seen.has(arr[i])) {
-//         seen.add(arr[i]);
-//         distinctCount++;
-//       }
-//     }
-  
-//     return distinctCount;
-//   }
-  
-//   // Ví dụ sử dụng
-//   const arr = [1, 3, 4, 5, 5, 2, 4, 8, 1, 2, 3];
-//   const distinctValues = countDistinctValues(arr);
-//   console.log(distinctValues); // Output: 6
 
-// Test 2 Tìm giá trị trùng nhau
+// var totalCoin = course.reduce(function coinHeadler(total, course) {
+//   return total + course.coin
+// }, 0);
 
-// function findDuplicates(arr) {
-//   const duplicates = [];
-//   const countObj = {};
-
-//   // Đếm số lần xuất hiện của mỗi phần tử
-//   for (let i = 0; i < arr.length; i++) {
-//     const num = arr[i];
-//     countObj[num] = (countObj[num] || 0) + 1;
-//   }
-
-//   // Lấy các phần tử có số lần xuất hiện nhiều hơn 1
-//   for (const num in countObj) {
-//     if (countObj[num] > 1) {
-//       duplicates.push(num);
-//     }
-//   }
-
-//   return duplicates;
-// }
-
-// const myArray = [1, 1, 3, 5, 6, 3, 4, 5, 7, 8, 2, 3, 5, 6, 7, 8, 9, 6, 5, 4, 3];
-// const duplicateElements = findDuplicates(myArray);
-// console.log(duplicateElements); // Output: ['1', '3', '5', '6', '4', '7', '8']
-
-//Test 3 Tìm giá trị khác nhau
-
-// function findUniqueElements(arr) {
-//   const uniqueElements = new Set(arr);
-//   return Array.from(uniqueElements);
-// }
-
-// const myArray = [1, 1, 3, 5, 6, 3, 4, 5, 7, 8, 2, 3, 5, 6, 7, 8, 9, 6, 5, 4, 3];
-// const uniqueElements = findUniqueElements(myArray);
-// console.log(uniqueElements); // Output: [1, 3, 5, 6, 4, 7, 8, 2, 9]
-
-//Test 4 đếm số lần xuất hiện của các phần tử
-
-// function countElements(arr) {
-//   const countObj = {};
-
-//   // Đếm số lần xuất hiện của mỗi phần tử
-//   for (let i = 0; i < arr.length; i++) {
-//     const num = arr[i];
-//     countObj[num] = (countObj[num] || 0) + 1;
-//   }
-
-//   // Liệt kê số lần xuất hiện của các phần tử
-//   const result = [];
-//   for (const num in countObj) {
-//     result.push(`Phần tử ${num} xuất hiện ${countObj[num]} lần`);
-//   }
-
-//   return result;
-// }
-
-// const myArray = [1, 1, 3, 5, 6, 3, 4, 5, 7, 8, 2, 3, 5, 6, 7, 8, 9, 6, 5, 4, 3];
-// const countResult = countElements(myArray);
-// console.log(countResult);
+// console.log(totalCoin)
